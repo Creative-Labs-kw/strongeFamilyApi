0ac1e2850f85d9651782c49b9eca4d61e0dd20af
Kw.Hades <darkside944@gmail.com>
2023-04-05 13:19:32 +0300

    Fix & Make Schemas

---
+++ models/Family.ts
@@ -0,0 +1,19 @@
+import mongoose, { Schema, model, Document } from "mongoose";
+
+export interface IFamily extends Document {
+  familyName: string;
+  familyMember: mongoose.Schema.Types.ObjectId;
+}
+
+const familySchema = new Schema<IFamily>({
+  familyName: {
+    type: String,
+    required: false,
+  },
+  familyMember: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
+
+export default model<IFamily>("Family", familySchema);
