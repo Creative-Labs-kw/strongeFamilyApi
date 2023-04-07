9b4aa18888c7c890e4d45c503f46c44e06c2fa58
Kw.Hades <darkside944@gmail.com>
2023-04-07 22:07:44 +0300

    Fix Scehema

--- models/Family.ts
+++ models/Family.ts
@@ -2,23 +2,12 @@ import mongoose, { Schema, model, Document } from "mongoose";
 
 export interface IFamily extends Document {
   familyName: string;
-  familyMember: mongoose.Schema.Types.ObjectId;
+  familyMember: mongoose.Types.ObjectId[];
 }
 
-const familySchema = new Schema<IFamily>(
-  {
-    familyName: {
-      type: String,
-      required: false,
-    },
-    familyMember: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: "User",
-      },
-    ],
-  },
-  { timestamps: true }
-);
+const familySchema = new Schema<IFamily>({
+  familyName: { type: String, required: false },
+  familyMember: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+});
 
 export default model<IFamily>("Family", familySchema);
--- models/User.ts
+++ models/User.ts
@@ -1,59 +1,35 @@
-import mongoose, { Schema, model, Document } from "mongoose";
+import mongoose from "mongoose";
 
-export interface INotification extends Document {
-  message: string;
-  isRead: boolean;
-  userId: mongoose.Schema.Types.ObjectId;
-}
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-export interface IUser extends Document {
+export interface IUser extends mongoose.Document {
   name: string;
   email: string;
   password: string;
-  store?: mongoose.Schema.Types.ObjectId;
-  isAdmin?: boolean;
-  notifications?: INotification[];
+  createdAt: Date;
+  updatedAt: Date;
 }
 
-const userSchema = new Schema<IUser>(
-  {
-    name: {
-      type: String,
-      required: false,
-    },
-    email: {
-      type: String,
-      required: false,
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: false,
-    },
-    store: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Store", //!Make it later
-    },
-    isAdmin: {
-      type: Boolean,
-      default: false,
-    },
-    notifications: [
-      {
-        message: {
-          type: String,
-          required: false,
-        },
-        isRead: {
-          type: Boolean,
-          default: false,
-        },
-      },
-    ],
-  },
-  {
-    timestamps: true,
-  }
-);
-
-export default model<IUser>("User", userSchema);
+export const User = mongoose.model<IUser>("User", userSchema);
