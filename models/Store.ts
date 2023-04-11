1d5c08ac90636c41d5047961abb70a2d4684f86a
Kw.Hades <darkside944@gmail.com>
2023-04-11 19:46:40 +0300

    Add store schema

---
+++ models/Store.ts
@@ -0,0 +1,14 @@
+import mongoose, { Schema, model, Document } from "mongoose";
+import { IUser } from "../models/User";
+
+export interface IStore extends Document {
+  storeName: string;
+  owner: mongoose.Types.ObjectId | IUser; // reference to the User schema
+}
+
+const storeSchema = new Schema<IStore>({
+  storeName: { type: String, required: true },
+  owner: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: false },
+});
+
+export default model<IStore>("Store", storeSchema);
