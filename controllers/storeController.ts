09ceec0061d3f0302aed92a3a348ebbaa0681237
Kw.Hades <darkside944@gmail.com>
2023-04-17 21:24:55 +0300

    Edit create & Update func

--- controllers/storeController.ts
+++ controllers/storeController.ts
@@ -2,7 +2,7 @@ import logger from "../utils/logger";
 import { Document } from "mongoose";
 import Store, { IStore } from "../models/Store";
 import { Request, Response } from "express";
-import { IUser } from "../models/User";
+import { IUser, User } from "../models/User";
 interface UserDocument extends Document, IUser {}
 
 interface UserRequest extends Request {
@@ -18,27 +18,44 @@ export const updateStoreById = async (req: UserRequest, res: Response) => {
   try {
     // Find store by id and populate the owner field
     let store = await Store.findById(req.params.id).populate("owner");
-
-    logger.StoreLogger.error(
-      `Store with id ${req.params.id} found: ${JSON.stringify(store)}`
-    );
+    console.log("Found store:", store);
 
     if (!store) {
-      logger.StoreLogger.error(`Store with id ${req.params.id} not found`);
+      console.log(`Store with id ${req.params.id} not found`);
       res.status(404).json({ errors: [{ msg: "Store not found" }] });
       return;
     }
 
     // Check if the current user owns the store
-    if (store.owner && store.owner.toString() !== req.user?.id) {
-      logger.StoreLogger.error(`User not authorized to update store`);
+    if (store.owner && store.owner._id.toString() !== req.user?.id) {
+      console.log(`User not authorized to update store`);
       res.status(401).json({ errors: [{ msg: "User not authorized" }] });
       return;
     }
 
-    // Update store name
-    if (req.body.storeName) {
-      store.storeName = req.body.storeName;
+    // Update store properties
+    if (
+      req.body.storeName ||
+      req.body.address ||
+      req.body.phoneNumber ||
+      req.body.imageUrl ||
+      req.body.description
+    ) {
+      if (req.body.storeName) {
+        store.storeName = req.body.storeName;
+      }
+      if (req.body.address) {
+        store.address = req.body.address;
+      }
+      if (req.body.phoneNumber) {
+        store.phoneNumber = req.body.phoneNumber;
+      }
+      if (req.body.imageUrl) {
+        store.imageUrl = req.body.imageUrl;
+      }
+      if (req.body.description) {
+        store.description = req.body.description;
+      }
     }
 
     // Save updated store to database
@@ -68,7 +85,7 @@ export const getAllStores = async (
 
 //$ Create a new store
 export const createStore = async (req: UserRequest, res: Response) => {
-  const { storeName } = req.body;
+  const { storeName, address, phoneNumber, imageUrl, description } = req.body;
 
   try {
     // Check if store already exists
@@ -83,13 +100,23 @@ export const createStore = async (req: UserRequest, res: Response) => {
     store = new Store({
       storeName,
       owner: req.user?.id, // set the owner field to the current user's ID
+      address,
+      phoneNumber,
+      imageUrl,
+      description,
     });
 
     // Save store to database
     await store.save();
 
+    // Add store ID to user's stores array
+    await User.findByIdAndUpdate(
+      req.user?.id,
+      { $push: { stores: store._id } },
+      { new: true }
+    );
     // Return store object
-    res.json(store);
+    res.status(201).json(store);
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server error");
