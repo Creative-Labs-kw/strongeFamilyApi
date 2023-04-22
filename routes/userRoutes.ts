9312ccb62ba68ca51b5adb8db81a4469993fba2c
Kw.Hades <darkside944@gmail.com>
2023-04-22 07:30:02 +0300

    Add routes to new func's

--- routes/userRoutes.ts
+++ routes/userRoutes.ts
@@ -7,6 +7,7 @@ import {
   login,
   register,
   updateUserById,
+  updateUserStoresById,
 } from "../controllers/userController";
 import { authMiddleware } from "../middleware/passport";
 
@@ -17,6 +18,7 @@ userRouter.get("/:id", getUserById);
 userRouter.post("/login", login); //$ SignIn
 userRouter.post("/register", register); //$ SignUp
 userRouter.put("/:id", updateUserById);
+userRouter.put("/:id/stores/:storeId", updateUserStoresById);
 userRouter.delete("/:id", deleteUserById);
 userRouter.delete("/", deleteAllUsers);
 
