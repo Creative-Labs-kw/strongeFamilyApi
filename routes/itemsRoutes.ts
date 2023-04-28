e61dbc73df6db0c353173094a7ebec61247ae269
Kw.Hades <darkside944@gmail.com>
2023-04-28 16:06:36 +0300

    Add items routes

---
+++ routes/itemsRoutes.ts
@@ -0,0 +1,21 @@
+import { Router } from "express";
+import {
+  createItem,
+  deleteAllItems,
+  deleteItemById,
+  getAllItems,
+  getItemById,
+  updateItemById,
+} from "../controllers/itemController";
+// import { authMiddleware } from "../middleware/passport";
+
+const itemsRouter = Router();
+
+itemsRouter.get("/:storeId", getAllItems);
+itemsRouter.get("/:storeId/:itemId", getItemById);
+itemsRouter.post("/:storeId", createItem);
+itemsRouter.put("/:storeId/:itemId", updateItemById);
+itemsRouter.delete("/:storeId/:itemId", deleteItemById);
+itemsRouter.delete("/:storeId/", deleteAllItems);
+
+export default itemsRouter;
