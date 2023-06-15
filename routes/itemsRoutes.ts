a817370c04f6964248ceb09518d9bc472a0aa201
Kw.Hades <darkside944@gmail.com>
2023-06-15 08:52:21 +0300

    Add AuthMiddleware

--- routes/itemsRoutes.ts
+++ routes/itemsRoutes.ts
@@ -7,14 +7,15 @@ import {
   getItemById,
   updateItemById,
 } from "../controllers/itemController";
+import { authMiddleware } from "../middleware/passport";
 
 const itemsRouter = Router();
 
 itemsRouter.get("/:storeId", getAllItems);
 itemsRouter.get("/:storeId/:itemId", getItemById);
-itemsRouter.post("/:storeId/createItems", createItem);
-itemsRouter.put("/:storeId/:itemId", updateItemById);
-itemsRouter.delete("/:storeId/:itemId", deleteItemById);
+itemsRouter.post("/:storeId/createItems", authMiddleware, createItem);
+itemsRouter.put("/:storeId/:itemId", authMiddleware, updateItemById);
+itemsRouter.delete("/:storeId/:itemId", authMiddleware, deleteItemById);
 itemsRouter.delete("/:storeId", deleteAllItems);
 
 export default itemsRouter;
