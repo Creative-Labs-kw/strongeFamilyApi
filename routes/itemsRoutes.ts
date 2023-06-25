13ebe061fbc6705ec9a7f7716cb52fc422cb17c7
Kw.Hades <darkside944@gmail.com>
2023-06-25 23:47:07 +0300

    Convert mongoose to firebase

--- routes/itemsRoutes.ts
+++ routes/itemsRoutes.ts
@@ -7,15 +7,14 @@ import {
   getItemById,
   updateItemById,
 } from "../controllers/itemController";
-import { authMiddleware } from "../middleware/passport";
 
 const itemsRouter = Router();
 
 itemsRouter.get("/:storeId", getAllItems);
 itemsRouter.get("/:storeId/:itemId", getItemById);
-itemsRouter.post("/:storeId/createItems", authMiddleware, createItem);
-itemsRouter.put("/:storeId/:itemId", authMiddleware, updateItemById);
-itemsRouter.delete("/:storeId/:itemId", authMiddleware, deleteItemById);
+itemsRouter.post("/:storeId/createItems", createItem);
+itemsRouter.put("/:storeId/:itemId", updateItemById);
+itemsRouter.delete("/:storeId/:itemId", deleteItemById);
 itemsRouter.delete("/:storeId", deleteAllItems);
 
 export default itemsRouter;
