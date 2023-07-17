b5ea336d6f4cfd4129cd39f4c515f9bbbbf11963
Kw.Hades <darkside944@gmail.com>
2023-07-17 07:30:54 +0300

    Add chats functions

--- routes/chatRoutes.ts
+++ routes/chatRoutes.ts
@@ -3,12 +3,18 @@ import {
   getChatById,
   getAllChats,
   sendChat,
+  getChatMessages,
+  deleteAllChats,
+  createChat,
 } from "../controllers/chatController";
 
 const chatRouter = Router();
 
-chatRouter.post("/send", sendChat);
-chatRouter.get("/getChatById", getChatById);
+chatRouter.post("/send/:userId", sendChat);
+chatRouter.post("/createChat/:userId", createChat);
+chatRouter.get("/getChatById/:userId/:chatId", getChatById);
 chatRouter.get("/getAllChats", getAllChats);
+chatRouter.get("/getChatMessages/:chatId/:userId", getChatMessages);
+chatRouter.delete("/deleteAllChats/:userId", deleteAllChats);
 
 export default chatRouter;
